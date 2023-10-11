6e04f5bbf038c8c9091d6799a4649523be56de95
Mohamed Ashraf <mohmedashraf.2004@gmail.com>
2023-10-11 22:28:18 +0300

    aritmetic_operations

---
+++ arithmetic_operations.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+   int x=5;
+   int y=2;
+
+   int sum;
+   sum=x+y;
+   printf("sum of two numbers = %d\n",sum);
+
+   int difference;
+   difference=x-y;
+   printf("difference of two numbers = %d\n",difference);
+
+   int product;
+   product=x*y;
+   printf("product of two numbers = %d\n", product);
+
+   float quotient;
+   quotient=x/y;
+   printf("quotient of two numbers = %f\n", quotient);
+
+   int remainder;
+   remainder=x%y;
+   printf("remainder of two numbers = %d\n", remainder);
+
+
+}
+
