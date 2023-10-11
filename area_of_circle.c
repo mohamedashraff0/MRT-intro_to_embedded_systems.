c0f91fc091eadc875625e1d1df9d115e7d830143
Mohamed Ashraf <mohmedashraf.2004@gmail.com>
2023-10-11 22:27:06 +0300

    area

---
+++ area_of_circle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+ float r;
+ float area;
+
+ r=5.2;           /* r= radius of the circle */
+ area=r*r*3.14;
+
+ printf("area of a circle = %f",area);
+
+
+}
